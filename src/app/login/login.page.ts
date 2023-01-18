76a5c7e8285ae7789b8f6973ec849925da4644fb
Alan Gonzalo Ramos <alanramos_18@hotmail.com>
2023-01-18 00:36:35 -0300

    added changes to login.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
-import { LoginService } from '../services/login.service';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -20,10 +20,12 @@ export class LoginPage implements OnInit {
   }
 
   login() {
-    console.log('El user es: ', this.username);
-    console.log('El pass es: ', this.password);
+    console.log('El username es: ', this.username);
+    console.log('El password es: ', this.password);
     this.loginService.login(this.username, this.password).subscribe(res => {
-      console.log('La respues fue: ', res);
+      console.log('La respuesta fue: ', res);
     });
+
+    console.log('Llega aca');
   }
 }
